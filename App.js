36a080fc108cec42783e6d8fe1c8340986174574
Dennis <dennis@Denniss-MBP.lan>
2022-12-06 14:26:34 -0600

    android app icon updated: unit swap updated

--- App.js
+++ App.js
@@ -6,118 +6,164 @@
  * @flow strict-local
  */
 
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 // import type { Node } from 'react';
-import { StatusBar, Text, TextInput, View } from 'react-native';
-import { allUnits, unitMatch } from './utils';
+import {StatusBar, Text, TextInput, View} from 'react-native';
+import {allUnits, unitMatch} from './utils';
 import SelectDropdown from 'react-native-select-dropdown';
-import { Button, IconButton } from 'react-native-paper';
-
+import {Button, IconButton} from 'react-native-paper';
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
 
 const App = () => {
-  const [num1, setNum1] = useState("0");
-  const [num2, setNum2] = useState("0");
-  const [selectedUnit1, setSelectedUnit1] = useState("mg");
-  const [selectedUnit2, setSelectedUnit2] = useState("mcg");
-  const [rightUnitList, setRightUnitList] = useState(["mcg", "grain", "gram"]);
+  const [num1, setNum1] = useState('0');
+  const [num2, setNum2] = useState('0');
+  const [selectedUnit1, setSelectedUnit1] = useState('mg');
+  const [selectedUnit2, setSelectedUnit2] = useState('mcg');
+  const [rightUnitList, setRightUnitList] = useState(['mcg', 'grain', 'gram']);
+  // const [swap, setSwap] = useState(false);
 
-  const selectLeftItem = (selectedItem) =>{
+  const selectLeftItem = selectedItem => {
     setSelectedUnit1(selectedItem);
     const keys = Object.keys(unitMatch(selectedItem));
-    console.log(unitMatch(selectedItem));
-    console.log(keys);
-    setRightUnitList(keys)
+    setRightUnitList(keys);
     setSelectedUnit2(keys[0]);
-  }
+  };
 
   const convert = () => {
     const float1 = Number.parseFloat(num1);
-    console.log(float1);
-    console.log(selectedUnit1);
-    console.log(selectedUnit2);
-    console.log(float1*unitMatch(selectedUnit1)[selectedUnit2]);
-    const float2 = float1*unitMatch(selectedUnit1)[selectedUnit2];
+    const float2 = float1 * unitMatch(selectedUnit1)[selectedUnit2];
     setNum2(float2.toString());
   };
 
-  const switchNums = () => {
-    const temp = Number.parseFloat(num1).toString();
+  const switchNumsAndUnits = () => {
+    const tempNumber = Number.parseFloat(num1).toString();
     setNum1(Number.parseFloat(num2).toString());
-    setNum2(temp);
+    setNum2(tempNumber);
+
+    const tempUnit = selectedUnit1;
+    selectLeftItem(selectedUnit2);
+    setSelectedUnit2(tempUnit);
   };
 
   return (
     <View style={{backgroundColor: '#fafafa', height: '100%'}}>
       <StatusBar barStyle={'default'} backgroundColor={'grey'} />
-      <View style={{ backgroundColor: '', marginTop: '10%' }}>
-        <Text style={{ fontSize: 20, alignSelf: 'center', color: 'grey' }}>Dosage Converter</Text>
+      <View style={{backgroundColor: '', marginTop: '10%'}}>
+        <Text style={{fontSize: 25, alignSelf: 'center', color: 'black'}}>
+          Dosage Converter
+        </Text>
       </View>
 
       {/* line 1 for numbers */}
-      <View style={{ flexDirection: 'row', marginTop: 20 }}>
-        <View style={{ width: "40%" }}>
-          <TextInput placeholder='Number 1' keyboardType='numeric' selectTextOnFocus={true} value={num1} onChangeText={(e) => setNum1(e)} style={{ borderWidth: 1, width: '95%', alignSelf:'flex-end', backgroundColor: "white", color: 'black'}} />
+      <View style={{flexDirection: 'row', marginTop: 50}}>
+        <View style={{width: '40%'}}>
+          <TextInput
+            placeholder="Number 1"
+            keyboardType="numeric"
+            selectTextOnFocus={true}
+            value={num1}
+            onChangeText={e => setNum1(e)}
+            style={{
+              borderWidth: 1,
+              width: '95%',
+              alignSelf: 'flex-end',
+              backgroundColor: 'white',
+              color: 'black',
+            }}
+          />
         </View>
 
-        <View style={{ width: "20%"}}>
-          <IconButton icon={'arrow-left-right-bold'} style={{position: 'absolute', width: '50%', height: '50%', margin: 'auto', marginLeft: "20%", marginTop: '15%'}} onPress={switchNums} />
+        <View style={{width: '20%'}}>
+          <IconButton
+            icon={'arrow-left-right-bold'}
+            style={{
+              position: 'absolute',
+              width: '50%',
+              height: '50%',
+              margin: 'auto',
+              marginLeft: '20%',
+              marginTop: '15%',
+            }}
+            onPress={switchNumsAndUnits}
+          />
         </View>
 
-        <View style={{ width: "40%" }}>
-          <TextInput editable={false} placeholder='Number 2' keyboardType='numeric' value={num2} onChangeText={(e) => setNum2(e)} style={{ borderWidth: 1, width: '95%', alignSelf:"flex-start", color: 'black', backgroundColor: '#e1e1e0'}} />
+        <View style={{width: '40%'}}>
+          <TextInput
+            editable={false}
+            placeholder="Number 2"
+            keyboardType="numeric"
+            value={num2}
+            onChangeText={e => setNum2(e)}
+            style={{
+              borderWidth: 1,
+              width: '95%',
+              alignSelf: 'flex-start',
+              color: 'black',
+              backgroundColor: '#e1e1e0',
+            }}
+          />
         </View>
       </View>
 
       {/* line 2 for unit selections */}
-      <View style={{ flexDirection: 'row' }}>
-        <View style={{ width: "40%" }}>
+      <View style={{flexDirection: 'row'}}>
+        <View style={{width: '40%'}}>
           <SelectDropdown
-            buttonStyle={{ width: "95%", alignSelf: 'flex-end' }}
+            buttonStyle={{width: '95%', alignSelf: 'flex-end'}}
+            selectedRowStyle={{backgroundColor: '#20b1aa'}}
+            selectedRowTextStyle={{color: 'white'}}
             data={allUnits}
-            onSelect={(selectedItem) => {
+            onSelect={selectedItem => {
               selectLeftItem(selectedItem);
             }}
             buttonTextAfterSelection={(selectedItem, index) => {
               // text represented after item is selected
               // if data array is an array of objects then return selectedItem.property to render after item is selected
-              return selectedItem
+              return selectedItem;
             }}
             rowTextForSelection={(item, index) => {
               // text represented for each item in dropdown
               // if data array is an array of objects then return item.property to represent item in dropdown
-              return item
+              return item;
             }}
-            defaultValue={allUnits[0]}
+            defaultValue={selectedUnit1}
           />
         </View>
-        <View style={{ width: '20%' }} />
-        <View style={{ width: "40%" }}>
+        <View style={{width: '20%'}} />
+        <View style={{width: '40%'}}>
           <SelectDropdown
-            buttonStyle={{ width: "95%", alignSelf: 'flex-start' }}
+            buttonStyle={{width: '95%', alignSelf: 'flex-start'}}
+            selectedRowStyle={{backgroundColor: '#20b1aa'}}
+            selectedRowTextStyle={{color: 'white'}}
             data={rightUnitList}
-            onSelect={(selectedItem) => {
-              setSelectedUnit2(selectedItem)
+            onSelect={selectedItem => {
+              setSelectedUnit2(selectedItem);
             }}
             buttonTextAfterSelection={(selectedItem, index) => {
               // text represented after item is selected
               // if data array is an array of objects then return selectedItem.property to render after item is selected
-              return selectedItem
+              return selectedItem;
             }}
             rowTextForSelection={(item, index) => {
               // text represented for each item in dropdown
               // if data array is an array of objects then return item.property to represent item in dropdown
-              return item
+              return item;
             }}
-            defaultValue={rightUnitList[0]}
+            defaultValue={selectedUnit2}
           />
         </View>
       </View>
 
-      <View style={{ marginTop: '10%', width: '98%', alignSelf: 'center' }}>
-        <Button mode='contained' onPress={convert} style={{backgroundColor: '#20b1aa'}} >Convert</Button>
+      <View style={{marginTop: '10%', width: '98%', alignSelf: 'center'}}>
+        <Button
+          mode="contained"
+          onPress={convert}
+          style={{backgroundColor: '#20b1aa'}}>
+          Convert
+        </Button>
       </View>
     </View>
   );
--- utils.js
+++ utils.js
@@ -1,4 +1,18 @@
-export const allUnits = ["mg", "mcg", "teaspoon", "tablespoon", "oz", "ml", "cup", "quart", "pint", "grain", "gram", "kg", "lb"];
+export const allUnits = [
+  'mg',
+  'mcg',
+  'teaspoon',
+  'tablespoon',
+  'oz',
+  'ml',
+  'cup',
+  'quart',
+  'pint',
+  'grain',
+  'gram',
+  'kg',
+  'lb',
+];
 
 // 1 mg -> 1000 mcg
 // 1 teaspoon -> 5 ml
@@ -11,22 +25,22 @@ export const allUnits = ["mg", "mcg", "teaspoon", "tablespoon", "oz", "ml", "cup
 // 1 gram (g) -> 1000 mg
 // 1 kg -> 22 lbs
 // 1 lbs -> 16 oz
-export const unitMatch = (selectedUnit)=>{
-    const dictionary = {
-        mg: {mcg: 1000, grain: 1/60, gram: 1/1000},
-        mcg: {mg: 1/1000},
-        teaspoon: {ml: 5, tablespoon: 1/3},
-        ml: {teaspoon: 1/5, oz: 1/30},
-        tablespoon: {teaspoon: 3, ml: 15},
-        oz: {ml: 30, cup: 1/8, lb: 1/16, pint: 1/16},
-        cup: {ml: 240, oz: 8, pint: 1/2, quart: 1/4},
-        quart: {ml: 960, pint: 2, cup: 4, oz: 32},
-        pint: {ml: 480, quart: 1/2, cup: 2, oz: 16},
-        lb: {oz: 16, kg: 1/22},
-        kg: {lb: 22},
-        grain: {mg: 60, mcg: 60000, gram: 60/1000},
-        gram: {mg: 1000, mcg: 1000000, grain: 1000/60},
-    }
+export const unitMatch = selectedUnit => {
+  const dictionary = {
+    mg: {mcg: 1000, grain: 1 / 60, gram: 1 / 1000},
+    mcg: {mg: 1 / 1000},
+    teaspoon: {ml: 5, tablespoon: 1 / 3},
+    ml: {tablespoon: 1 / 15, teaspoon: 1 / 5, oz: 1 / 30},
+    tablespoon: {teaspoon: 3, ml: 15},
+    oz: {ml: 30, cup: 1 / 8, lb: 1 / 16, pint: 1 / 16},
+    cup: {ml: 240, oz: 8, pint: 1 / 2, quart: 1 / 4},
+    quart: {ml: 960, pint: 2, cup: 4, oz: 32},
+    pint: {ml: 480, quart: 1 / 2, cup: 2, oz: 16},
+    lb: {oz: 16, kg: 1 / 2.205},
+    kg: {lb: 2.205},
+    grain: {mg: 60, mcg: 60000, gram: 60 / 1000},
+    gram: {mg: 1000, mcg: 1000000, grain: 1000 / 60},
+  };
 
-    return dictionary[selectedUnit];
-}
+  return dictionary[selectedUnit];
+};
